63b854ec23fed93c9c7331a2abe8d7aea3abe0d1
Daniel Sateler <satelerd@gmail.com>
2024-11-19 18:10:06 -0300

    add first page and connection to backend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,21 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        "primary-dark": "var(--primary-dark)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+        card: "var(--card)",
+        "card-foreground": "var(--card-foreground)",
+        muted: "var(--muted)",
+        "muted-foreground": "var(--muted-foreground)",
+        border: "var(--border)",
       },
     },
   },
